8672d9d0b8850708b3a87ae520c7f49c4a5c4139
momeng00 <69623137+momeng00@users.noreply.github.com>
2025-06-09 19:26:51 +0900

    공중 애니메이션 체크
    Charcter의 상태를 공중에서 분할시켜놨다.
    Jump와 Falling과 Fallen으로 isGround는 나중에 외부로 빼야함.
    애니메이션과 묶어서 하나의 상태를 확인하면서 애니메이션을 작동시킬꺼임
    Test용

--- Assets/02.Script/InputSystem/InputTest.cs
+++ Assets/02.Script/InputSystem/InputTest.cs
@@ -2,8 +2,11 @@ using System.Collections.Generic;
 using UnityEngine;
 public class InputTest : MonoBehaviour
 {
-    public float _x;
-    public Rigidbody2D rb;
+    private float _x;
+    private Rigidbody2D rb;
+    public Vector2 vec;
+    public CharacterState characterState;
+    public float jumpForce;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -11,15 +14,48 @@ public class InputTest : MonoBehaviour
     }
     private void Update()
     {
-        rb.position += new Vector2(_x*3.2f*Time.deltaTime,0.0f);
+        rb.position += new Vector2(_x*3.2f*Time.deltaTime,0f);
+        if (rb.linearVelocityY > 0.05f)
+        {
+            characterState = CharacterState.Jump;
+        }
+        else if (rb.linearVelocityY < -0.005f)
+        {
+            if (characterState == CharacterState.Falling)
+                return;
+            characterState = CharacterState.Falling;
+            vec.y = transform.position.y;
+        }
+        else if (vec.y - transform.position.y > 8.0f)
+        {
+            characterState = CharacterState.Fallen;
+        }
+        else
+        {
+            vec.y = Vector2.zero.y;
+            characterState = CharacterState.Ground;
+        }
+
     }
     public void initialize()
     {
         InputSystem.Instance.RegisterAction(KeyState.Play_Key,"Horizontal",Move);
+        InputSystem.Instance.RegisterAction(KeyState.Play_Key, KeyCode.Space, Jump);
     }
     public void Move(float x)
     {
         _x = x;
     }
-        
+    public void Jump()
+    {
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+      
+}
+public enum CharacterState
+{
+    Ground,
+    Jump,
+    Falling,
+    Fallen,
 }
