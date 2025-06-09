85850888b4a5b2ebdc232da1bc5dd939e5eaa201
momeng00 <69623137+momeng00@users.noreply.github.com>
2025-06-09 19:59:28 +0900

    Fall을 Falling과 Fallen으로 분할
    Unity 옵션 설정

--- Assets/02.Script/InputSystem/InputTest.cs
+++ Assets/02.Script/InputSystem/InputTest.cs
@@ -4,12 +4,14 @@ public class InputTest : MonoBehaviour
 {
     private float _x;
     private Rigidbody2D rb;
+    private Animator ani;
     public Vector2 vec;
     public CharacterState characterState;
     public float jumpForce;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
         initialize();
     }
     private void Update()
@@ -17,21 +19,25 @@ public class InputTest : MonoBehaviour
         rb.position += new Vector2(_x*3.2f*Time.deltaTime,0f);
         if (rb.linearVelocityY > 0.05f)
         {
+            ani.SetInteger("State",1);
             characterState = CharacterState.Jump;
         }
         else if (rb.linearVelocityY < -0.005f)
         {
             if (characterState == CharacterState.Falling)
                 return;
+            ani.SetInteger("State", 2);
             characterState = CharacterState.Falling;
             vec.y = transform.position.y;
         }
         else if (vec.y - transform.position.y > 8.0f)
         {
+            ani.SetInteger("State", 3);
             characterState = CharacterState.Fallen;
         }
         else
         {
+            ani.SetInteger("State", 0);
             vec.y = Vector2.zero.y;
             characterState = CharacterState.Ground;
         }
