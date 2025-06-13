37055bbddc7d6059ae5ce14d44cd1920bdab98e3
momeng00 <69623137+momeng00@users.noreply.github.com>
2025-06-13 09:06:42 +0900

    벽면 마찰력 0
    캐릭터의 이동 매커니즘을 변경해서 마찰력 자체를 삭제해싴.
    다른 오브젝트또한 마찰력이 필요하므로 수정이 필요

--- Assets/02.Script/Character/CharacterControl.cs
+++ Assets/02.Script/Character/CharacterControl.cs
@@ -1,41 +1,78 @@
 using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class CharacterControl : MonoBehaviour
 {
     
     private Rigidbody2D _rb;
     private Animator _ani;
-
-    private float _axisX;
-    private float _axisY;
-    private Vector2 _move;
+    
+    private float _axisX; //이동되는 수치 받을 힘
+    private float _axisY; //이동되는 수치 받을 힘
+    private float _direction;
     private Vector2 _prePos;
+    public bool isGrounded
+    {
+        get
+        {
+            Collider2D col;
+            col = Physics2D.OverlapBox(_rb.position + footOffset, 
+                new Vector2(transform.localScale.x, 0.2f),
+                0.0f,
+                groundMask);
+            return col;
+        }
+    }
 
-    private bool _isGrounded;
     private bool _canMove;
     private bool _isDetected;
 
     private AerialState _aerialState;
 
+    public float moveSpeed;
+    public float jumpForce;
+    public Vector2 footOffset;
     public LayerMask detectMask;
     public LayerMask groundMask;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Initialize();
     }
-
+    private void Awake()
+    {
+        _canMove = true;
+        _ani = GetComponent<Animator>();
+        _rb = GetComponent<Rigidbody2D>();
+        _axisY = 0;
+        _axisX = 0;
+    }
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    private void Move()
+    private void FixedUpdate()
+    {
+        _rb.linearVelocity = new Vector2(_axisX * moveSpeed , _rb.linearVelocityY);
+    }
+    private void Move(float horizontal)
     {
         if(!_canMove)
             return;
-
+        _axisX = horizontal;
     }
+    private void Jump()
+    {
+        _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void Initialize()
+    {
+        InputSystem.Instance.RegisterAction(KeyState.Play_Key,"Horizontal",Move);
+        InputSystem.Instance.RegisterAction(KeyState.Play_Key, KeyCode.Space, Jump);
+    }
+
 }
 public enum AerialState
 {
