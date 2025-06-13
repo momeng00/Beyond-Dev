b94d688fe00fd7a2c53115c6193d724ceffe25cd
momeng00 <69623137+momeng00@users.noreply.github.com>
2025-06-13 09:49:14 +0900

    Update InputSystem.cs

--- Assets/02.Script/InputSystem/InputSystem.cs
+++ Assets/02.Script/InputSystem/InputSystem.cs
@@ -62,19 +62,22 @@ public class InputSystem : MonoBehaviour
 
     public void RegisterAction(KeyState state, KeyCode keyCode, Action act)
     {
-        if (maps[state]._actionKey.ContainsKey(keyCode))
+        
+        if (!maps[state]._actionKey.ContainsKey(keyCode))
         {
-            maps[state]._actionKey[keyCode] += act;
+            maps[state]._actionKey.Add(keyCode, act);
+            return;
         }
-        maps[state]._actionKey.Add(keyCode, act);
+            maps[state]._actionKey[keyCode] += act;
     }
     public void RegisterAction(KeyState state, string axis, Action<float> act)
     {
-        if (maps[state]._actionAxis.ContainsKey(axis))
+        if (!maps[state]._actionAxis.ContainsKey(axis))
         {
-            maps[state]._actionAxis[axis] += act;
+            maps[state]._actionAxis.Add(axis, act);
+            return;
         }
-        maps[state]._actionAxis.Add(axis, act);
+            maps[state]._actionAxis[axis] += act;
     }
     public void DeregisterAction(KeyState state, KeyCode keyCode, Action act)
     {
